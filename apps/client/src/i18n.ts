fae7951db3d603576050e512e1c4d960a7c03707
MrSteel83 <trueffel-menagen.0g@icloud.com>
2025-05-26 20:05:38 +0200

    Update i18n.ts
    fallback language changed to german

--- apps/client/src/i18n.ts
+++ apps/client/src/i18n.ts
@@ -12,7 +12,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: "en-US",
+    fallbackLng: "de-DE",
     debug: false,
     load: 'currentOnly',
 
