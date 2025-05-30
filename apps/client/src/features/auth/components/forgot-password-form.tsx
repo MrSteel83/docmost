b8311089748b01649c3b0a8007cce64251a4fccd
MrSteel83 <trueffel-menagen.0g@icloud.com>
2025-05-31 00:32:24 +0200

    Update forgot-password-form.tsx
    logo added

--- apps/client/src/features/auth/components/forgot-password-form.tsx
+++ apps/client/src/features/auth/components/forgot-password-form.tsx
@@ -37,6 +37,14 @@ export function ForgotPasswordForm() {
   return (
     <Container size={420} className={classes.container}>
       <Box p="xl" className={classes.containerBox}>
+        <Box mb="md" style={{ textAlign: "center" }}>
+            <img
+              src="form_logo.png"
+              height={40}
+              style={{ objectFit: "contain" }}
+            />
+          </Box>
+        
         <Title order={2} ta="center" fw={500} mb="md">
           {t("Forgot password")}
         </Title>
