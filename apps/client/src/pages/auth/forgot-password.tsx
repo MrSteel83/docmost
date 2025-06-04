36bed99b32aecd08c6b4ceeb5e8e25bb77905e12
MrSteel83 <trueffel-menagen.0g@icloud.com>
2025-06-04 02:19:16 +0200

    Update forgot-password.tsx
    trans lib added

--- apps/client/src/pages/auth/forgot-password.tsx
+++ apps/client/src/pages/auth/forgot-password.tsx
@@ -1,6 +1,7 @@
 import { ForgotPasswordForm } from "@/features/auth/components/forgot-password-form";
 import { getAppName } from "@/lib/config";
 import { Helmet } from "react-helmet-async";
+import { useTranslation } from "react-i18next";
 
 export default function ForgotPassword() {
     return (
