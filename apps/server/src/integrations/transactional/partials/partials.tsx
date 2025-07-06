8bf1ba360455366c5d54e9a0f83c1c194582f783
MrSteel83 <trueffel-menagen.0g@icloud.com>
2025-07-06 22:59:28 +0200

    Update partials.tsx
    env vars added to support links in email

--- apps/server/src/integrations/transactional/partials/partials.tsx
+++ apps/server/src/integrations/transactional/partials/partials.tsx
@@ -17,7 +17,10 @@ interface MailBodyProps {
 // text over environment variables
 const emailTexts = {
   text1: process.env.EMAIL_FOOTER_T1 || 'Docmost, All Rights Reserved',
-  text2: process.env.EMAIL_FOOTER_T2 || '',
+  link1: process.env.EMAIL_FOOTER_LINK_URL1 || '',
+  linktext1: process.env.EMAIL_FOOTER_LINK_TEXT1 || '',
+  link2: process.env.EMAIL_FOOTER_LINK_URL2 || '',
+  linktext2: process.env.EMAIL_FOOTER_LINK_TEXT2 || '',
 };
 
 export function MailBody({ children }: MailBodyProps) {
@@ -46,7 +49,14 @@ export function MailFooter() {
     <Section style={footer}>
       <Row>
         <Text style={{ textAlign: 'center', color: '#706a7b' }}>
-          {emailTexts.text1}<br /><br />{emailTexts.text2}
+          {emailTexts.text1}
+          <br/>
+          <Link href={emailTexts.link1}>
+            {emailTexts.linktext1}
+          </Link>{' | '}
+          <Link href={emailTexts.link2}>
+            {emailTexts.linktext2}
+          </Link>
         </Text>
       </Row>
     </Section>
