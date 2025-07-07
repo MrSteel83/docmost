8e12d8acca96e4bfc2b3183e8528d7fb2a099a7e
MrSteel83 <trueffel-menagen.0g@icloud.com>
2025-07-07 16:00:17 +0200

    Update change-password-email.tsx
    mail template optimized with env vars

--- apps/server/src/integrations/transactional/emails/change-password-email.tsx
+++ apps/server/src/integrations/transactional/emails/change-password-email.tsx
@@ -1,19 +1,27 @@
 import { Section, Text } from '@react-email/components';
 import * as React from 'react';
-import { content, paragraph } from '../css/styles';
+import { content, paragraph, h1 } from '../css/styles';
 import { MailBody } from '../partials/partials';
 
 interface Props {
   username?: string;
 }
 
+// text over environment variables
+const emailTexts = {
+  text1: process.env.EMAIL_PW_CHANGED_T1 || 'Password changed successfully',
+  text2: process.env.EMAIL_PW_CHANGED_T2 || 'Hi ',
+  text3: process.env.EMAIL_PW_CHANGED_T3 || 'This is a confirmation that your password has been changed.',
+};
+
 export const ChangePasswordEmail = ({ username }: Props) => {
   return (
     <MailBody>
       <Section style={content}>
-        <Text style={paragraph}>Hi {username},</Text>
+        <Text style={h1}>{emailTexts.text1}</Text>
+        <Text style={paragraph}>{emailTexts.text2}{username},</Text>
         <Text style={paragraph}>
-          This is a confirmation that your password has been changed.
+          {emailTexts.text3}
         </Text>
       </Section>
     </MailBody>
