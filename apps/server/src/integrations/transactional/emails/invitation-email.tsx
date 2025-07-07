eb659ae0edbfb7bbd63968f10a41a8fb8a1634fc
MrSteel83 <trueffel-menagen.0g@icloud.com>
2025-07-07 16:14:44 +0200

    Update invitation-email.tsx
    mail template optimized with env vars

--- apps/server/src/integrations/transactional/emails/invitation-email.tsx
+++ apps/server/src/integrations/transactional/emails/invitation-email.tsx
@@ -1,21 +1,29 @@
 import { Section, Text, Button } from '@react-email/components';
 import * as React from 'react';
-import { button, content, paragraph } from '../css/styles';
+import { button, content, paragraph, h1 } from '../css/styles';
 import { MailBody } from '../partials/partials';
 
 interface Props {
   inviteLink: string;
 }
 
+// text over environment variables
+const emailTexts = {
+  text1: process.env.EMAIL_IN_SEND_T1 || 'Your invitation',
+  text2: process.env.EMAIL_IN_SEND_T2 || 'Hi there,',
+  text3: process.env.EMAIL_IN_SEND_T3 || 'You have been invited to Docmost.',
+  text4: process.env.EMAIL_IN_SEND_T4 || 'Please click the button below to accept this invitation.',
+  text5: process.env.EMAIL_IN_SEND_T5 || 'Accept Invite'
+};
+
 export const InvitationEmail = ({ inviteLink }: Props) => {
   return (
     <MailBody>
       <Section style={content}>
-        <Text style={paragraph}>Hi there,</Text>
-        <Text style={paragraph}>You have been invited to Docmost.</Text>
-        <Text style={paragraph}>
-          Please click the button below to accept this invitation.
-        </Text>
+        <Text style={h1}>{emailTexts.text1}</Text>
+        <Text style={paragraph}>{emailTexts.text2}</Text>
+        <Text style={paragraph}>{emailTexts.text3}</Text>
+        <Text style={paragraph}>{emailTexts.text4}</Text>
       </Section>
       <Section
         style={{
@@ -27,7 +35,7 @@ export const InvitationEmail = ({ inviteLink }: Props) => {
         }}
       >
         <Button href={inviteLink} style={button}>
-          Accept Invite
+          {emailTexts.text5}
         </Button>
       </Section>
     </MailBody>
